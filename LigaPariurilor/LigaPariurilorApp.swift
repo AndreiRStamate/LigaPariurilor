3012939c1bef31318b92ebf2994c3839d55990bb
Andrei R Stamate <a144185i@gmail.com>
2025-04-16 13:14:22 +0300

    Initial Commit

---
+++ LigaPariurilor/LigaPariurilorApp.swift
@@ -0,0 +1,17 @@
+//
+//  LigaPariurilorApp.swift
+//  LigaPariurilor
+//
+//  Created by Andrei R Stamate on 16.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct LigaPariurilorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LigaPariurilorTests/LigaPariurilorTests.swift
@@ -0,0 +1,17 @@
+//
+//  LigaPariurilorTests.swift
+//  LigaPariurilorTests
+//
+//  Created by Andrei R Stamate on 16.04.2025.
+//
+
+import Testing
+@testable import LigaPariurilor
+
+struct LigaPariurilorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
